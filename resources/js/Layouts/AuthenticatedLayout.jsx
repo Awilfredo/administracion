767ba45d3a49288dcbd320e0cc81623cd5f90158
Awilfredo <davidernestotores@gmail.com>
2024-08-02 16:04:57 -0600

    Dashboard add cards

--- resources/js/Layouts/AuthenticatedLayout.jsx
+++ resources/js/Layouts/AuthenticatedLayout.jsx
@@ -214,11 +214,14 @@ export default function Authenticated({ user, header, children }) {
                                             </span>
                                         </Link>
                                     </li>
+
                                     <li>
                                         <Link
                                             href={route("estadisticas.index")}
                                             className={
-                                                route().current("estadisticas.index")
+                                                route().current(
+                                                    "estadisticas.index"
+                                                )
                                                     ? navSelectedClass
                                                     : navDefaultClass
                                             }
@@ -243,33 +246,35 @@ export default function Authenticated({ user, header, children }) {
                                             </span>
                                         </Link>
                                     </li>
-                                    <li>
-                                        <a
-                                            href="#"
-                                            className="px-4 py-3 flex items-center space-x-4 rounded-md text-gray-600 group"
-                                        >
-                                            <svg
-                                                xmlns="http://www.w3.org/2000/svg"
-                                                className="h-5 w-5"
-                                                viewBox="0 0 20 20"
-                                                fill="currentColor"
+                                    {false && (
+                                        <li>
+                                            <a
+                                                href="#"
+                                                className="px-4 py-3 flex items-center space-x-4 rounded-md text-gray-600 group"
                                             >
-                                                <path
-                                                    className="fill-current text-gray-600 group-hover:text-cyan-600"
-                                                    fillRule="evenodd"
-                                                    d="M2 5a2 2 0 012-2h8a2 2 0 012 2v10a2 2 0 002 2H4a2 2 0 01-2-2V5zm3 1h6v4H5V6zm6 6H5v2h6v-2z"
-                                                    clipRule="evenodd"
-                                                />
-                                                <path
-                                                    className="fill-current text-gray-300 group-hover:text-cyan-300"
-                                                    d="M15 7h1a2 2 0 012 2v5.5a1.5 1.5 0 01-3 0V7z"
-                                                />
-                                            </svg>
-                                            <span className="group-hover:text-gray-700">
-                                                Reportes
-                                            </span>
-                                        </a>
-                                    </li>
+                                                <svg
+                                                    xmlns="http://www.w3.org/2000/svg"
+                                                    className="h-5 w-5"
+                                                    viewBox="0 0 20 20"
+                                                    fill="currentColor"
+                                                >
+                                                    <path
+                                                        className="fill-current text-gray-600 group-hover:text-cyan-600"
+                                                        fillRule="evenodd"
+                                                        d="M2 5a2 2 0 012-2h8a2 2 0 012 2v10a2 2 0 002 2H4a2 2 0 01-2-2V5zm3 1h6v4H5V6zm6 6H5v2h6v-2z"
+                                                        clipRule="evenodd"
+                                                    />
+                                                    <path
+                                                        className="fill-current text-gray-300 group-hover:text-cyan-300"
+                                                        d="M15 7h1a2 2 0 012 2v5.5a1.5 1.5 0 01-3 0V7z"
+                                                    />
+                                                </svg>
+                                                <span className="group-hover:text-gray-700">
+                                                    Reportes
+                                                </span>
+                                            </a>
+                                        </li>
+                                    )}
                                 </ul>
                             </li>
 
@@ -299,34 +304,35 @@ export default function Authenticated({ user, header, children }) {
                                     </span>
                                 </Link>
                             </li>
-
-                            <li>
-                                <a
-                                    href="#"
-                                    className="px-4 py-3 flex items-center space-x-4 rounded-md text-gray-600 group"
-                                >
-                                    <svg
-                                        xmlns="http://www.w3.org/2000/svg"
-                                        className="h-5 w-5"
-                                        viewBox="0 0 20 20"
-                                        fill="currentColor"
+                            {false && (
+                                <li>
+                                    <a
+                                        href="#"
+                                        className="px-4 py-3 flex items-center space-x-4 rounded-md text-gray-600 group"
                                     >
-                                        <path
-                                            className="fill-current text-gray-300 group-hover:text-cyan-300"
-                                            d="M4 4a2 2 0 00-2 2v1h16V6a2 2 0 00-2-2H4z"
-                                        />
-                                        <path
-                                            className="fill-current text-gray-600 group-hover:text-cyan-600"
-                                            fillRule="evenodd"
-                                            d="M18 9H2v5a2 2 0 002 2h12a2 2 0 002-2V9zM4 13a1 1 0 011-1h1a1 1 0 110 2H5a1 1 0 01-1-1zm5-1a1 1 0 100 2h1a1 1 0 100-2H9z"
-                                            clipRule="evenodd"
-                                        />
-                                    </svg>
-                                    <span className="group-hover:text-gray-700">
-                                        Finance
-                                    </span>
-                                </a>
-                            </li>
+                                        <svg
+                                            xmlns="http://www.w3.org/2000/svg"
+                                            className="h-5 w-5"
+                                            viewBox="0 0 20 20"
+                                            fill="currentColor"
+                                        >
+                                            <path
+                                                className="fill-current text-gray-300 group-hover:text-cyan-300"
+                                                d="M4 4a2 2 0 00-2 2v1h16V6a2 2 0 00-2-2H4z"
+                                            />
+                                            <path
+                                                className="fill-current text-gray-600 group-hover:text-cyan-600"
+                                                fillRule="evenodd"
+                                                d="M18 9H2v5a2 2 0 002 2h12a2 2 0 002-2V9zM4 13a1 1 0 011-1h1a1 1 0 110 2H5a1 1 0 01-1-1zm5-1a1 1 0 100 2h1a1 1 0 100-2H9z"
+                                                clipRule="evenodd"
+                                            />
+                                        </svg>
+                                        <span className="group-hover:text-gray-700">
+                                            Finance
+                                        </span>
+                                    </a>
+                                </li>
+                            )}
                             <li>
                                 <Link
                                     href={route("profile.edit")}
