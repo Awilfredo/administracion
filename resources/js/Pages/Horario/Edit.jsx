2d1da1cebc8596ddff9d874e34194697a9369179
Awilfredo <davidernestotores@gmail.com>
2024-08-13 17:36:42 -0600

    Editar dias de horarios vista

--- resources/js/Pages/Horario/Edit.jsx
+++ resources/js/Pages/Horario/Edit.jsx
@@ -1,34 +1,52 @@
 import AuthenticatedLayout from "@/Layouts/AuthenticatedLayout";
 import { Head } from "@inertiajs/react";
 import EditDay from "./Partials/EditDay";
-import { useState } from "react";
-import AddIcon from "@/Components/Icons/AddIcon";
+import { useEffect, useState } from "react";
 import CardAdd from "@/Components/CardAdd";
 import Modal from "@/Components/Modal";
-import PrimaryButtonBlue from "@/Components/PrimaryButtonBlue";
-import Matrix from "@/Components/Matrix";
+import { ManejoFechas } from "@/Helpers/ManejoFechas";
+import DangerButton from "@/Components/DangerButton";
 function Edit({ auth, horario }) {
     const [dias, setdias] = useState([...horario.dias]);
     const [showSelectDay, setshowSelectDay] = useState(false);
-    console.log(dias);
-    const handleAddDay = () => {
-        if(dias.length<6){
-            setshowSelectDay(true);
+    const [diasDisponibles, setDiasDisponibles] = useState([]);
+    const { diasDeLaSemana } = ManejoFechas();
+    useEffect(() => {
+        const dias_ocupados = [];
+        dias.forEach((element) => {
+            dias_ocupados.push(element.numero_dia);
+        });
+        const resultados = diasDeLaSemana.filter(
+            (obj) => !dias_ocupados.includes(obj.value)
+        );
+        setDiasDisponibles(resultados);
+    }, [dias]);
 
-        }
+    const handleAddDay = () => {
+        setshowSelectDay(true);
+        // if(dias.length<6){
+        //     setshowSelectDay(true);
+
+        // }
+    };
+
+    const handleCreateDay = (dia) => {
         setdias([
             ...dias,
             {
-                numero_dia: dias.length + 1,
+                numero_dia: dia,
                 entrada: "",
                 salida: "",
                 entrada_almuerzo: "",
                 salida_almuerzo: "",
             },
         ]);
+
+        setshowSelectDay(false);
     };
-
-
+    const CancelSelectDay = (e) => {
+        setshowSelectDay(false);
+    };
 
     return (
         <AuthenticatedLayout
@@ -43,27 +61,44 @@ function Edit({ auth, horario }) {
 
             <div className="w-full sm:px-2 md:px-10 justify-center pb-10">
                 {dias.map((element, index) => (
-                    <EditDay dia={element}></EditDay>
+                    <EditDay dia={element} key={index}></EditDay>
                 ))}
 
                 {dias.length < 7 ? (
-                    <CardAdd onClick={handleAddDay}><p className="mt-5">Agregar nuevo dia al horario</p></CardAdd>
+                    <CardAdd onClick={handleAddDay}>
+                        <p className="mt-5">Agregar nuevo dia al horario</p>
+                    </CardAdd>
                 ) : (
                     ""
                 )}
             </div>
 
-            <Modal show={showSelectDay} closeable={true}>
+            <Modal
+                show={showSelectDay}
+                closeable={true}
+                onClose={CancelSelectDay}
+            >
                 <div className="p-5">
-                    <strong>Selecciona un dia:</strong>
-                    <div className="flex">
-                        <PrimaryButtonBlue>{}</PrimaryButtonBlue>
+                    <strong>Selecciona un dia disponible:</strong>
+                    <div className="flex mt-5 justify-around flex-wrap">
+                        {diasDisponibles.map((element, index) => (
+                            <button
+                                onClick={()=>handleCreateDay(element.value)}
+                                className="mx-2 bg-gray-200 px-5 py-2 mt-5 rounded hover:bg-gray-500 hover:text-white"
+                                key={index}
+                            >
+                                {element.name}
+                            </button>
+                        ))}
                     </div>
                 </div>
+
+                <div className="w-full flex justify-end p-5">
+                    <DangerButton type="button" onClick={CancelSelectDay}>
+                        Cancelar
+                    </DangerButton>
+                </div>
             </Modal>
-
-            <Matrix></Matrix>
-
         </AuthenticatedLayout>
     );
 }
